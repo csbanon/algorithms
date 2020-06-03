f518acd418ef1c3a8cbc5e44beb927be8977cb2f
Carlos Santiago Bañón <46613416+csbanon@users.noreply.github.com>
2020-06-02 21:57:02 -0400

    Remove class.

--- rescue-boats.py
+++ rescue-boats.py
@@ -49,14 +49,13 @@ Space Complexity:
 O(N)
 """
 
-class RescueBoats(object):
-  def numRescueBoats(self, people, limit):
-    people.sort()
-    i, j = 0, len(people) - 1
-    ans = 0
-    while i <= j:
-      ans += 1
-      if people[i] + people[j] <= limit:
-        i += 1
-      j -= 1
-    return ans
+def numRescueBoats(self, people, limit):
+  people.sort()
+  i, j = 0, len(people) - 1
+  ans = 0
+  while i <= j:
+    ans += 1
+    if people[i] + people[j] <= limit:
+      i += 1
+    j -= 1
+  return ans
